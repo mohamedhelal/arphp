9f47a0bbdacc45ba2495413ee159f4b607439e4c
mohamed helal <mohamedhelal123456@gmail.com>
2017-01-02 02:40:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * -----------------------------------
+ * File  : index.php
+ * User  : Mohamed Helal
+ * Email : mohamedhelal123456@gmail.com
+ * Site  : {URL}
+ * -----------------------------------
+ */
+
+$app = require_once 'boot/Application.php';
+$app->run();
